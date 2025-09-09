4012e5cf8360bce8ea1bc1e397db6acbb2f04ab3
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2025-09-09 14:41:56 +0200

    09-09-2025

--- ClimData/codes/mk_meteorologic_plots.cmd
+++ ClimData/codes/mk_meteorologic_plots.cmd
@@ -1,50 +1,38 @@
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt01/ "La Paya" 01_LaPaya
-mv 01_LaPaya*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt01/ "La Paya" 01_LaPaya
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt02/ "Rosaria de Lerma" 02_RosariodeLerma
-mv 02_RosariodeLerma*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt02/ "Rosaria de Lerma" 02_RosariodeLerma
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt03/ "Cafayate" 03_Cafayate
-mv 03_Cafayate*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt03/ "Cafayate" 03_Cafayate
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt04/ "Angastaco" 04_Angastaco
-mv 04_Angastaco*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt04/ "Angastaco" 04_Angastaco
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt05/ "La Poma" 05_LaPoma
-mv 05_LaPoma*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt05/ "La Poma" 05_LaPoma
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt06/ "Santa Maria" 06_SantaMaria
-mv 06_SantaMaria*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt06/ "Santa Maria" 06_SantaMaria
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt07/ "Las Cardones" 07_LosCardones
-mv 07_LosCardones.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt07/ "Las Cardones" 07_LosCardones
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt08/ Alfarcito 08_Alfarcito
-mv 08_Alfarcito*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt08/ Alfarcito 08_Alfarcito
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt09/ "San Antonio de los Cobres" 09_SAdelosCobres
-mv 09_SAdelosCobres*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt09/ "San Antonio de los Cobres" 09_SAdelosCobres
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt10/ "Tolar Grandes" 10_TolarGrandes
-mv 10_TolarGrandes*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt10/ "Tolar Grandes" 10_TolarGrandes
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt11/ "Salar Llullaillaco" 11_SalarLlullaillaco
-mv 11_SalarLlullaillaco*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt11/ "Salar Llullaillaco" 11_SalarLlullaillaco
 
 cd /home/bodo/vacon.github.io/ClimData/
-python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon.github.io/ClimData/clim_data/qt12/ "San Jose de Metan" 12_Metan
-mv 12_Metan*.png /home/bodo/vacon.github.io/ClimData/
+python3 /home/bodo/vacon.github.io/ClimData/codes/read_meteorologic_data.py /home/bodo/vacon/clim_data/qt12/ "San Jose de Metan" 12_Metan
 
 
 
