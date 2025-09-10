e4fa096c41b52b31212b4825d9c24d5b6bb812c8
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2025-09-10 20:37:08 +0200

    09-10-2025

---
+++ ClimData/codes/mk_meteorologic_plots_cachi.cmd
@@ -0,0 +1,36 @@
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt01/ "La Paya" 01_LaPaya
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt02/ "Rosaria de Lerma" 02_RosariodeLerma
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt03/ "Cafayate" 03_Cafayate
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt04/ "Angastaco" 04_Angastaco
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt05/ "La Poma" 05_LaPoma
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt06/ "Santa Maria" 06_SantaMaria
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt07/ "Las Cardones" 07_LosCardones
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt08/ Alfarcito 08_Alfarcito
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt09/ "San Antonio de los Cobres" 09_SAdelosCobres
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt10/ "Tolar Grande" 10_TolarGrande
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt11/ "Salar Llullaillaco" 11_SalarLlullaillaco
+
+cd /raid/nwarg/vacon.github.io/ClimData/
+python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_meteorologic_data.py /raid/nwarg/vacon/clim_data/qt12/ "San Jose de Metan" 12_Metan
+
