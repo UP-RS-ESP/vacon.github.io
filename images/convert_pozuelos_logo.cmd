08523562fded3e24e1e9652edf1d02d5638440b6
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2026-09-23 09:44:44 +0200

    09-23-2026

---
+++ images/convert_pozuelos_logo.cmd
@@ -0,0 +1,1 @@
+magick pozuelos_logo.png -background white -resize 1200x 1200_pozuelos_logo.jpg
--- images/mk_logos.cmd
+++ images/mk_logos.cmd
@@ -1,3 +1,4 @@
-convert -quality 99 -density 300 UP_1200x_logo.png IBIGEO_1200x_logo.jpg 1200px-Emblema_Parque_Nacional_Los_Cardones.jpg -fuzz 1% -trim -bordercolor white -border 800x0 +repage +append 3logos_1200x.jpg
-convert 3logos_1200x.jpg -resize 600x 3logos_1200x.jpg
-convert -quality 99 -density 300 3logos_1200x.jpg NWArg_GNSS_VACON_fixedSA.jpg -fuzz 1% -trim -bordercolor white -border 0x10 +repage -gravity center -append 3Logo_map_VACON.jpg
+magick -quality 99 -density 300 UP_1200x_logo.png IBIGEO_1200x_logo.jpg 1200px-Emblema_Parque_Nacional_Los_Cardones.jpg 1200_pozuelos_logo.jpg -fuzz 1% -trim -bordercolor white -border 800x0 +repage +append 4logos_1200x.jpg
+magick /Dropbox/Argentina/NGL/NWArg_GNSS_VACON_fixedSA.png -resize 600x NWArg_GNSS_VACON_fixedSA.jpg 
+magick 4logos_1200x.jpg -resize 600x 4logos_1200x.jpg
+magick -quality 99 -density 300 4logos_1200x.jpg NWArg_GNSS_VACON_fixedSA.jpg -fuzz 1% -trim -bordercolor white -border 0x10 +repage -gravity center -append 4Logo_map_VACON.jpg
